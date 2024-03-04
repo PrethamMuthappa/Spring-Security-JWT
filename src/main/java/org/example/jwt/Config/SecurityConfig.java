43d0caa7e43e9f67f39d5ff15eed089855001876
PrethamMuthappa <prethambachira1819@gmail.com>
2024-03-04 18:44:17 +0530

    Security config

---
+++ src/main/java/org/example/jwt/Config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package org.example.jwt.Config;
+
+
+import org.example.jwt.Filter.JwtFilter;
+import org.example.jwt.Service.UserDetailsServices;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.config.AuditingConfiguration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig {
+
+    protected  final UserDetailsServices userDetailsServices;
+    protected final JwtFilter jwtFilter;
+
+    public SecurityConfig(UserDetailsServices userDetailsServices, JwtFilter jwtFilter) {
+        this.userDetailsServices = userDetailsServices;
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        req->req.requestMatchers("/login/**","/register/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).userDetailsService(userDetailsServices)
+                .sessionManagement(sessio->sessio.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+
+    @Bean
+    public  PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+}
+
+
